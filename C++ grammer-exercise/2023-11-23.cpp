ebe88fe581270904bffe9180f2bb140a6e825206
ZzW163010 <146351364+ZzW163010@users.noreply.github.com>
2023-11-23 21:45:50 +0800

    Create 2023-11-23.cpp

---
+++ C++ grammer-exercise/2023-11-23.cpp
@@ -0,0 +1,119 @@
+//字符串移位包含问题
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    string a, b;
+    
+    cin >> a >> b;
+    if(a.size() < b.size()) swap(a, b);
+    
+    for(int i = 0; i < a.size(); i++){
+        a = a.substr(1) + a[0];
+        
+        for(int j = 0; j + b.size() <= a.size(); j++){
+            int k = 0;
+            
+            for(k = 0; k < b.size(); k++){
+                if(a[j + k] != b[k]) break;
+                }
+                if(k == b.size()){
+                    cout << "true" << endl;
+                    return 0;
+            }
+        }
+    }
+    cout << "false" << endl;
+}
+
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string a, b;
+    
+    cin >> a >> b;
+    if (a.size() < b.size()) swap(a, b);
+        
+    for (int i = 0; i < a.size(); i++) {
+        a = a.substr(1) + a[0];
+
+        for (int k = 0; k + b.size() <= a.size(); k++) {
+            for (int j = k; j < a.size(); j++) {
+                if (a.substr(k, j + 1 - k) == b) {
+                    cout << "true" << endl;
+                    return 0;
+                }
+            }
+        }
+    }
+    cout << "false" << endl;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    if (a.size() > b.size()) {
+        int i = 0, j = 0, cnt = 0;
+        for (i = 0; i < a.size(); i++) {
+            cnt = 0;
+            char tmp = a[0];
+            for (j = 1; j < a.size(); j++) a[j - 1] = a[j];
+            a[j - 1] = tmp;
+
+            int k = 0, m = 0;
+            for (k = 0; k < a.size(); k++) {
+                if (a[k] != b[m]) k++;
+                else {
+                    while (k < a.size() && m < b.size()) {
+                        if (a[k] == b[m]) cnt++;
+                        k++;
+                        m++;
+                    }
+                    if (cnt == b.size()) {
+                        cout << "true" << endl;
+                        return 0;
+                    }
+                    else break;
+                }
+            }
+        }
+    }
+    else {
+        int i = 0, j = 0, cnt = 0;
+        for (i = 0; i < b.size(); i++) {
+            cnt = 0;
+            char tmp = b[0];
+            for (j = 1; j < b.size(); j++) b[j - 1] = b[j];
+            b[j - 1] = tmp;
+            int k = 0, m = 0;
+            for (k = 0; k < b.size(); k++) {
+                while (k < b.size() && m < a.size()) {
+                    if (b[k] == a[m]) cnt++;
+                    k++;
+                    m++;
+                }
+                if (cnt == a.size()) {
+                    cout << "true" << endl;
+                    return 0;
+                }
+                else break;
+            }
+        }
+    }
+    cout << "false" << endl;
+    return 0;
+}
